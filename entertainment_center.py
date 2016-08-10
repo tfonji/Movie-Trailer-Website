94a9e22e3b6fc2dd2d8364230a3074d84452a1de
Tembong Fonji <fonji@email.arizona.edu>
2016-08-10 12:40:08 -0700

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,30 @@
+import media
+import fresh_tomatoes
+
+# With the imported class file, media, movies are created using the Movie() class.
+toy_story = media.Movie("Toy Story",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar",
+                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                      "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+
+school_of_rock = media.Movie("School of Rock",
+                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
+
+ratatouille = media.Movie("Ratatouille",
+                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris",
+                                "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=BYRWfS2s2v4")
+
+hunger_games = media.Movie("Hunger Games",
+                           "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                           "www.youtube.com/watch?v=qoUT7q2iTbQ")
+
+#movies below, stores the list of movies created above.
+movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
